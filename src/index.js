23b5e7f856685ac47c3642ae5440a1e90c266fe7
olteanurobert <olteanurobertcatalin@gmail.com>
2023-05-04 16:26:16 +0300

    Updated index.js with createRoot
    Updated index.js with createRoot

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+
 import App from "./App";
+
+import { createRoot } from "react-dom/client";
+
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
